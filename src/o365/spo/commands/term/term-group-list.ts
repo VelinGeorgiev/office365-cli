pnp/cli-microsoft365

Added the 'spo term group list' command solving #596

src/o365/spo/commands/term/term-group-list.ts
@@ -1,0 +1,129 @@
+import auth from '../../SpoAuth';
+import { ContextInfo, ClientSvcResponse, ClientSvcResponseContents } from '../../spo';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandError
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { TermGroupCollection } from './TermGroupCollection';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: GlobalOptions;
+}
+
+class SpoTermGroupListCommand extends SpoCommand {
+  public get name(): string {
+    return commands.TERM_GROUP_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists taxonomy term groups';
+  }
+
+  protected requiresTenantAdmin(): boolean {
+    return true;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: (err?: any) => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        if (this.verbose) {
+          cmd.log(`Retrieving taxonomy term groups...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="4" ObjectPathId="3" /><ObjectIdentityQuery Id="5" ObjectPathId="3" /><ObjectPath Id="7" ObjectPathId="6" /><ObjectIdentityQuery Id="8" ObjectPathId="6" /><ObjectPath Id="10" ObjectPathId="9" /><Query Id="11" ObjectPathId="9"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="3" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="6" ParentId="3" Name="GetDefaultSiteCollectionTermStore" /><Property Id="9" ParentId="6" Name="Groups" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          cb(new CommandError(response.ErrorInfo.ErrorMessage));
+          return;
+        }
+
+        const result: TermGroupCollection = json[json.length - 1];
+        if (result._Child_Items_ && result._Child_Items_.length > 0) {
+          if (args.options.output === 'json') {
+            cmd.log(result._Child_Items_.map(t => {
+              t.CreatedDate = t.CreatedDate.replace('/Date(', '').replace(')/', '');
+              t.Id = t.Id.replace('/Guid(', '').replace(')/', '');
+              t.LastModifiedDate = t.LastModifiedDate.replace('/Date(', '').replace(')/', '');
+              return t;
+            }));
+          }
+          else {
+            cmd.log(result._Child_Items_.map(t => {
+              return {
+                Id: t.Id.replace('/Guid(', '').replace(')/', ''),
+                Name: t.Name
+              };
+            }));
+          }
+        }
+        cb();
+      }, (err: any): void => this.handleRejectedPromise(err, cmd, cb));
+  }
+
+  public commandHelp(args: CommandArgs, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.TERM_GROUP_LIST).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, connect to a SharePoint Online tenant admin site,
+  using the ${chalk.blue(commands.CONNECT)} command.
+        
+  Remarks:
+
+    To list taxonomy term groups, you have to first connect to a tenant admin
+    site using the ${chalk.blue(commands.CONNECT)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.CONNECT} https://contoso-admin.sharepoint.com`)}.
+
+  Examples:
+  
+    List taxonomy term groups
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_LIST}
+`);
+  }
+}
+
+module.exports = new SpoTermGroupListCommand();

VelinGeorgiev/office365-cli

src/o365/spo/commands/term/term-group-list.ts
@@ -1,0 +1,129 @@
+import auth from '../../SpoAuth';
+import { ContextInfo, ClientSvcResponse, ClientSvcResponseContents } from '../../spo';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandError
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { TermGroupCollection } from './TermGroupCollection';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: GlobalOptions;
+}
+
+class SpoTermGroupListCommand extends SpoCommand {
+  public get name(): string {
+    return commands.TERM_GROUP_LIST;
+  }
+
+  public get description(): string {
+    return 'Lists taxonomy term groups';
+  }
+
+  protected requiresTenantAdmin(): boolean {
+    return true;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: (err?: any) => void): void {
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        if (this.verbose) {
+          cmd.log(`Retrieving taxonomy term groups...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="4" ObjectPathId="3" /><ObjectIdentityQuery Id="5" ObjectPathId="3" /><ObjectPath Id="7" ObjectPathId="6" /><ObjectIdentityQuery Id="8" ObjectPathId="6" /><ObjectPath Id="10" ObjectPathId="9" /><Query Id="11" ObjectPathId="9"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="3" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="6" ParentId="3" Name="GetDefaultSiteCollectionTermStore" /><Property Id="9" ParentId="6" Name="Groups" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): void => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          cb(new CommandError(response.ErrorInfo.ErrorMessage));
+          return;
+        }
+
+        const result: TermGroupCollection = json[json.length - 1];
+        if (result._Child_Items_ && result._Child_Items_.length > 0) {
+          if (args.options.output === 'json') {
+            cmd.log(result._Child_Items_.map(t => {
+              t.CreatedDate = t.CreatedDate.replace('/Date(', '').replace(')/', '');
+              t.Id = t.Id.replace('/Guid(', '').replace(')/', '');
+              t.LastModifiedDate = t.LastModifiedDate.replace('/Date(', '').replace(')/', '');
+              return t;
+            }));
+          }
+          else {
+            cmd.log(result._Child_Items_.map(t => {
+              return {
+                Id: t.Id.replace('/Guid(', '').replace(')/', ''),
+                Name: t.Name
+              };
+            }));
+          }
+        }
+        cb();
+      }, (err: any): void => this.handleRejectedPromise(err, cmd, cb));
+  }
+
+  public commandHelp(args: CommandArgs, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.TERM_GROUP_LIST).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, log in to SharePoint Online tenant admin site,
+    using the ${chalk.blue(commands.LOGIN)} command.
+        
+  Remarks:
+
+    To list taxonomy term groups, you have to first log in to a tenant admin
+    site using the ${chalk.blue(commands.LOGIN)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN} https://contoso-admin.sharepoint.com`)}.
+
+  Examples:
+  
+    List taxonomy term groups
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_LIST}
+`);
+  }
+}
+
+module.exports = new SpoTermGroupListCommand();