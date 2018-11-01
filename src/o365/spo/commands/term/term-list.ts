pnp/cli-microsoft365

Added the 'spo term list' command, solving #603

src/o365/spo/commands/term/term-list.ts
@@ -11,7 +11,7 @@ import {
 } from '../../../../Command';
 import SpoCommand from '../../SpoCommand';
 import Utils from '../../../../Utils';
-import { TermSetCollection } from './TermSetCollection';
+import { TermCollection } from './TermCollection';
 
 const vorpal: Vorpal = require('../../../../vorpal-init');
 
@@ -22,15 +22,17 @@ interface CommandArgs {
 interface Options extends GlobalOptions {
   termGroupId?: string;
   termGroupName?: string;
+  termSetId?: string;
+  termSetName?: string;
 }
 
-class SpoTermSetListCommand extends SpoCommand {
+class SpoTermListCommand extends SpoCommand {
   public get name(): string {
-    return commands.TERM_SET_LIST;
+    return commands.TERM_LIST;
   }
 
   public get description(): string {
-    return 'Lists taxonomy term sets from the given term group';
+    return 'Lists taxonomy terms from the given term set';
   }
 
   protected requiresTenantAdmin(): boolean {
@@ -41,6 +43,8 @@ class SpoTermSetListCommand extends SpoCommand {
     const telemetryProps: any = super.getTelemetryProperties(args);
     telemetryProps.termGroupId = typeof args.options.termGroupId !== 'undefined';
     telemetryProps.termGroupName = typeof args.options.termGroupName !== 'undefined';
+    telemetryProps.termSetId = typeof args.options.termSetId !== 'undefined';
+    telemetryProps.termSetName = typeof args.options.termSetName !== 'undefined';
     return telemetryProps;
   }
 
@@ -65,7 +69,8 @@ class SpoTermSetListCommand extends SpoCommand {
           cmd.log(`Retrieving taxonomy term sets...`);
         }
 
-        const query: string = args.options.termGroupId ? `<Method Id="62" ParentId="60" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termGroupId}}</Parameter></Parameters></Method>` : `<Method Id="62" ParentId="60" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termGroupName)}</Parameter></Parameters></Method>`
+        const termGroupQuery: string = args.options.termGroupId ? `<Method Id="77" ParentId="75" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termGroupId}}</Parameter></Parameters></Method>` : `<Method Id="77" ParentId="75" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termGroupName)}</Parameter></Parameters></Method>`;
+        const termSetQuery: string = args.options.termSetId ? `<Method Id="82" ParentId="80" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termSetId}}</Parameter></Parameters></Method>` : `<Method Id="82" ParentId="80" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termSetName)}</Parameter></Parameters></Method>`;
 
         const requestOptions: any = {
           url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
@@ -73,7 +78,7 @@ class SpoTermSetListCommand extends SpoCommand {
             authorization: `Bearer ${auth.service.accessToken}`,
             'X-RequestDigest': res.FormDigestValue
           }),
-          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="55" ObjectPathId="54" /><ObjectIdentityQuery Id="56" ObjectPathId="54" /><ObjectPath Id="58" ObjectPathId="57" /><ObjectIdentityQuery Id="59" ObjectPathId="57" /><ObjectPath Id="61" ObjectPathId="60" /><ObjectPath Id="63" ObjectPathId="62" /><ObjectIdentityQuery Id="64" ObjectPathId="62" /><ObjectPath Id="66" ObjectPathId="65" /><Query Id="67" ObjectPathId="65"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="54" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="57" ParentId="54" Name="GetDefaultSiteCollectionTermStore" /><Property Id="60" ParentId="57" Name="Groups" />${query}<Property Id="65" ParentId="62" Name="TermSets" /></ObjectPaths></Request>`
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="70" ObjectPathId="69" /><ObjectIdentityQuery Id="71" ObjectPathId="69" /><ObjectPath Id="73" ObjectPathId="72" /><ObjectIdentityQuery Id="74" ObjectPathId="72" /><ObjectPath Id="76" ObjectPathId="75" /><ObjectPath Id="78" ObjectPathId="77" /><ObjectIdentityQuery Id="79" ObjectPathId="77" /><ObjectPath Id="81" ObjectPathId="80" /><ObjectPath Id="83" ObjectPathId="82" /><ObjectIdentityQuery Id="84" ObjectPathId="82" /><ObjectPath Id="86" ObjectPathId="85" /><Query Id="87" ObjectPathId="85"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="69" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="72" ParentId="69" Name="GetDefaultSiteCollectionTermStore" /><Property Id="75" ParentId="72" Name="Groups" />${termGroupQuery}<Property Id="80" ParentId="77" Name="TermSets" />${termSetQuery}<Property Id="85" ParentId="82" Name="Terms" /></ObjectPaths></Request>`
         };
 
         if (this.debug) {
@@ -98,7 +103,7 @@ class SpoTermSetListCommand extends SpoCommand {
           return;
         }
 
-        const result: TermSetCollection = json[json.length - 1];
+        const result: TermCollection = json[json.length - 1];
         if (result._Child_Items_ && result._Child_Items_.length > 0) {
           if (args.options.output === 'json') {
             cmd.log(result._Child_Items_.map(t => {
@@ -125,11 +130,19 @@ class SpoTermSetListCommand extends SpoCommand {
     const options: CommandOption[] = [
       {
         option: '--termGroupId [termGroupId]',
-        description: 'ID of the term group from which to retrieve term sets. Specify termGroupName or termGroupId but not both'
+        description: 'ID of the term group where the term set is located. Specify termGroupId or termGroupName but not both'
       },
       {
         option: '--termGroupName [termGroupName]',
-        description: 'Name of the term group from which to retrieve term sets. Specify termGroupName or termGroupId but not both'
+        description: 'Name of the term group where the term set is located. Specify termGroupId or termGroupName but not both'
+      },
+      {
+        option: '--termSetId [termSetId]',
+        description: 'ID of the term set for which to retrieve terms. Specify termSetId or termSetName but not both'
+      },
+      {
+        option: '--termSetName [termSetName]',
+        description: 'Name of the term set for which to retrieve terms. Specify termSetId or termSetName but not both'
       }
     ];
 
@@ -153,32 +166,46 @@ class SpoTermSetListCommand extends SpoCommand {
         }
       }
 
+      if (!args.options.termSetId && !args.options.termSetName) {
+        return 'Specify either termSetId or termSetName';
+      }
+
+      if (args.options.termSetId && args.options.termSetName) {
+        return 'Specify either termSetId or termSetName but not both';
+      }
+
+      if (args.options.termSetId) {
+        if (!Utils.isValidGuid(args.options.termSetId)) {
+          return `${args.options.termSetId} is not a valid GUID`;
+        }
+      }
+
       return true;
     };
   }
 
   public commandHelp(args: CommandArgs, log: (help: string) => void): void {
     const chalk = vorpal.chalk;
-    log(vorpal.find(commands.TERM_SET_LIST).helpInformation());
+    log(vorpal.find(commands.TERM_LIST).helpInformation());
     log(
       `  ${chalk.yellow('Important:')} before using this command, log in to SharePoint Online tenant admin site,
     using the ${chalk.blue(commands.LOGIN)} command.
         
   Remarks:
 
-    To list taxonomy term sets, you have to first log in to a tenant admin
-    site using the ${chalk.blue(commands.LOGIN)} command,
+    To list taxonomy terms, you have to first log in to a tenant admin site
+    using the ${chalk.blue(commands.LOGIN)} command,
     eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN} https://contoso-admin.sharepoint.com`)}.
 
   Examples:
   
-    List taxonomy term sets from the term group with the given name
-      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupName PnPTermSets
+    List taxonomy terms from the term group and term set with the given name
+      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupName PnPTermSets --termSetName PnP-Organizations
 
-    List taxonomy term sets from the term group with the given ID
-      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupId 0e8f395e-ff58-4d45-9ff7-e331ab728beb
+    List taxonomy terms from the term group and term set with the given ID
+      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupId 0e8f395e-ff58-4d45-9ff7-e331ab728beb --termSetId 0e8f395e-ff58-4d45-9ff7-e331ab728bec
 `);
   }
 }
 
-module.exports = new SpoTermSetListCommand();
+module.exports = new SpoTermListCommand();

VelinGeorgiev/office365-cli

src/o365/spo/commands/term/term-list.ts
@@ -11,7 +11,7 @@ import {
 } from '../../../../Command';
 import SpoCommand from '../../SpoCommand';
 import Utils from '../../../../Utils';
-import { TermSetCollection } from './TermSetCollection';
+import { TermCollection } from './TermCollection';
 
 const vorpal: Vorpal = require('../../../../vorpal-init');
 
@@ -22,15 +22,17 @@ interface CommandArgs {
 interface Options extends GlobalOptions {
   termGroupId?: string;
   termGroupName?: string;
+  termSetId?: string;
+  termSetName?: string;
 }
 
-class SpoTermSetListCommand extends SpoCommand {
+class SpoTermListCommand extends SpoCommand {
   public get name(): string {
-    return commands.TERM_SET_LIST;
+    return commands.TERM_LIST;
   }
 
   public get description(): string {
-    return 'Lists taxonomy term sets from the given term group';
+    return 'Lists taxonomy terms from the given term set';
   }
 
   protected requiresTenantAdmin(): boolean {
@@ -41,6 +43,8 @@ class SpoTermSetListCommand extends SpoCommand {
     const telemetryProps: any = super.getTelemetryProperties(args);
     telemetryProps.termGroupId = typeof args.options.termGroupId !== 'undefined';
     telemetryProps.termGroupName = typeof args.options.termGroupName !== 'undefined';
+    telemetryProps.termSetId = typeof args.options.termSetId !== 'undefined';
+    telemetryProps.termSetName = typeof args.options.termSetName !== 'undefined';
     return telemetryProps;
   }
 
@@ -65,7 +69,8 @@ class SpoTermSetListCommand extends SpoCommand {
           cmd.log(`Retrieving taxonomy term sets...`);
         }
 
-        const query: string = args.options.termGroupId ? `<Method Id="62" ParentId="60" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termGroupId}}</Parameter></Parameters></Method>` : `<Method Id="62" ParentId="60" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termGroupName)}</Parameter></Parameters></Method>`
+        const termGroupQuery: string = args.options.termGroupId ? `<Method Id="77" ParentId="75" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termGroupId}}</Parameter></Parameters></Method>` : `<Method Id="77" ParentId="75" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termGroupName)}</Parameter></Parameters></Method>`;
+        const termSetQuery: string = args.options.termSetId ? `<Method Id="82" ParentId="80" Name="GetById"><Parameters><Parameter Type="Guid">{${args.options.termSetId}}</Parameter></Parameters></Method>` : `<Method Id="82" ParentId="80" Name="GetByName"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.termSetName)}</Parameter></Parameters></Method>`;
 
         const requestOptions: any = {
           url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
@@ -73,7 +78,7 @@ class SpoTermSetListCommand extends SpoCommand {
             authorization: `Bearer ${auth.service.accessToken}`,
             'X-RequestDigest': res.FormDigestValue
           }),
-          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="55" ObjectPathId="54" /><ObjectIdentityQuery Id="56" ObjectPathId="54" /><ObjectPath Id="58" ObjectPathId="57" /><ObjectIdentityQuery Id="59" ObjectPathId="57" /><ObjectPath Id="61" ObjectPathId="60" /><ObjectPath Id="63" ObjectPathId="62" /><ObjectIdentityQuery Id="64" ObjectPathId="62" /><ObjectPath Id="66" ObjectPathId="65" /><Query Id="67" ObjectPathId="65"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="54" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="57" ParentId="54" Name="GetDefaultSiteCollectionTermStore" /><Property Id="60" ParentId="57" Name="Groups" />${query}<Property Id="65" ParentId="62" Name="TermSets" /></ObjectPaths></Request>`
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="70" ObjectPathId="69" /><ObjectIdentityQuery Id="71" ObjectPathId="69" /><ObjectPath Id="73" ObjectPathId="72" /><ObjectIdentityQuery Id="74" ObjectPathId="72" /><ObjectPath Id="76" ObjectPathId="75" /><ObjectPath Id="78" ObjectPathId="77" /><ObjectIdentityQuery Id="79" ObjectPathId="77" /><ObjectPath Id="81" ObjectPathId="80" /><ObjectPath Id="83" ObjectPathId="82" /><ObjectIdentityQuery Id="84" ObjectPathId="82" /><ObjectPath Id="86" ObjectPathId="85" /><Query Id="87" ObjectPathId="85"><Query SelectAllProperties="false"><Properties /></Query><ChildItemQuery SelectAllProperties="true"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /></Properties></ChildItemQuery></Query></Actions><ObjectPaths><StaticMethod Id="69" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="72" ParentId="69" Name="GetDefaultSiteCollectionTermStore" /><Property Id="75" ParentId="72" Name="Groups" />${termGroupQuery}<Property Id="80" ParentId="77" Name="TermSets" />${termSetQuery}<Property Id="85" ParentId="82" Name="Terms" /></ObjectPaths></Request>`
         };
 
         if (this.debug) {
@@ -98,7 +103,7 @@ class SpoTermSetListCommand extends SpoCommand {
           return;
         }
 
-        const result: TermSetCollection = json[json.length - 1];
+        const result: TermCollection = json[json.length - 1];
         if (result._Child_Items_ && result._Child_Items_.length > 0) {
           if (args.options.output === 'json') {
             cmd.log(result._Child_Items_.map(t => {
@@ -125,11 +130,19 @@ class SpoTermSetListCommand extends SpoCommand {
     const options: CommandOption[] = [
       {
         option: '--termGroupId [termGroupId]',
-        description: 'ID of the term group from which to retrieve term sets. Specify termGroupName or termGroupId but not both'
+        description: 'ID of the term group where the term set is located. Specify termGroupId or termGroupName but not both'
       },
       {
         option: '--termGroupName [termGroupName]',
-        description: 'Name of the term group from which to retrieve term sets. Specify termGroupName or termGroupId but not both'
+        description: 'Name of the term group where the term set is located. Specify termGroupId or termGroupName but not both'
+      },
+      {
+        option: '--termSetId [termSetId]',
+        description: 'ID of the term set for which to retrieve terms. Specify termSetId or termSetName but not both'
+      },
+      {
+        option: '--termSetName [termSetName]',
+        description: 'Name of the term set for which to retrieve terms. Specify termSetId or termSetName but not both'
       }
     ];
 
@@ -153,32 +166,46 @@ class SpoTermSetListCommand extends SpoCommand {
         }
       }
 
+      if (!args.options.termSetId && !args.options.termSetName) {
+        return 'Specify either termSetId or termSetName';
+      }
+
+      if (args.options.termSetId && args.options.termSetName) {
+        return 'Specify either termSetId or termSetName but not both';
+      }
+
+      if (args.options.termSetId) {
+        if (!Utils.isValidGuid(args.options.termSetId)) {
+          return `${args.options.termSetId} is not a valid GUID`;
+        }
+      }
+
       return true;
     };
   }
 
   public commandHelp(args: CommandArgs, log: (help: string) => void): void {
     const chalk = vorpal.chalk;
-    log(vorpal.find(commands.TERM_SET_LIST).helpInformation());
+    log(vorpal.find(commands.TERM_LIST).helpInformation());
     log(
       `  ${chalk.yellow('Important:')} before using this command, log in to SharePoint Online tenant admin site,
     using the ${chalk.blue(commands.LOGIN)} command.
         
   Remarks:
 
-    To list taxonomy term sets, you have to first log in to a tenant admin
-    site using the ${chalk.blue(commands.LOGIN)} command,
+    To list taxonomy terms, you have to first log in to a tenant admin site
+    using the ${chalk.blue(commands.LOGIN)} command,
     eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN} https://contoso-admin.sharepoint.com`)}.
 
   Examples:
   
-    List taxonomy term sets from the term group with the given name
-      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupName PnPTermSets
+    List taxonomy terms from the term group and term set with the given name
+      ${chalk.grey(config.delimiter)} ${commands.TERM_LIST} --termGroupName PnPTermSets --termSetName PnP-Organizations
 
-    List taxonomy term sets from the term group with the given ID
-      ${chalk.grey(config.delimiter)} ${commands.TERM_SET_LIST} --termGroupId 0e8f395e-ff58-4d45-9ff7-e331ab728beb
+    List taxonomy terms from the term group and term set with the given ID
+      ${chalk.grey(config.delimiter)} ${commands.TERM_LIST} --termGroupId 0e8f395e-ff58-4d45-9ff7-e331ab728beb --termSetId 0e8f395e-ff58-4d45-9ff7-e331ab728bec
 `);
   }
 }
 
-module.exports = new SpoTermSetListCommand();
+module.exports = new SpoTermListCommand();