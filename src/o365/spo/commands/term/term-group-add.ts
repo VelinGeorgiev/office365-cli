pnp/cli-microsoft365

Added the 'spo term group add' command solving #598

src/o365/spo/commands/term/term-group-add.ts
@@ -1,0 +1,258 @@
+import auth from '../../SpoAuth';
+import { ContextInfo, ClientSvcResponse, ClientSvcResponseContents } from '../../spo';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+const uuidv4 = require('uuid/v4');
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandError,
+  CommandOption,
+  CommandValidate
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { TermStore } from './TermStore';
+import { TermGroup } from './TermGroup';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  description?: string;
+  id?: string;
+  name: string;
+}
+
+class SpoTermGroupAddCommand extends SpoCommand {
+  public get name(): string {
+    return commands.TERM_GROUP_ADD;
+  }
+
+  public get description(): string {
+    return 'Adds taxonomy term group';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.description = typeof args.options.id !== 'undefined';
+    telemetryProps.id = typeof args.options.id !== 'undefined';
+    return telemetryProps;
+  }
+
+  protected requiresTenantAdmin(): boolean {
+    return true;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: (err?: any) => void): void {
+    let formDigest: string = '';
+    let termGroup: TermGroup;
+
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        formDigest = res.FormDigestValue;
+
+        if (this.verbose) {
+          cmd.log(`Getting taxonomy term store...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="4" ObjectPathId="3" /><ObjectIdentityQuery Id="5" ObjectPathId="3" /><ObjectPath Id="7" ObjectPathId="6" /><ObjectIdentityQuery Id="8" ObjectPathId="6" /><Query Id="9" ObjectPathId="6"><Query SelectAllProperties="true"><Properties /></Query></Query></Actions><ObjectPaths><StaticMethod Id="3" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="6" ParentId="3" Name="GetDefaultSiteCollectionTermStore" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): request.RequestPromise | Promise<void> => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          return Promise.reject(response.ErrorInfo.ErrorMessage);
+        }
+
+        const termStore: TermStore = json[json.length - 1];
+        const termGroupId: string = args.options.id || uuidv4();
+
+        if (this.verbose) {
+          cmd.log(`Adding taxonomy term group...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': formDigest
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="14" ObjectPathId="13" /><ObjectIdentityQuery Id="15" ObjectPathId="13" /><Query Id="16" ObjectPathId="13"><Query SelectAllProperties="false"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /><Property Name="Description" ScalarProperty="true" /></Properties></Query></Query></Actions><ObjectPaths><Method Id="13" ParentId="6" Name="CreateGroup"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.name)}</Parameter><Parameter Type="Guid">{${termGroupId}}</Parameter></Parameters></Method><Identity Id="6" Name="${termStore._ObjectIdentity_}" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): request.RequestPromise | Promise<void> => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          return Promise.reject(response.ErrorInfo.ErrorMessage);
+        }
+
+        termGroup = json[json.length - 1];
+
+        if (!args.options.description) {
+          return Promise.resolve();
+        }
+
+        if (this.verbose) {
+          cmd.log(`Setting taxonomy term group description...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': formDigest
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><SetProperty Id="51" ObjectPathId="45" Name="Description"><Parameter Type="String">${Utils.escapeXml(args.options.description)}</Parameter></SetProperty></Actions><ObjectPaths><Identity Id="45" Name="${termGroup._ObjectIdentity_}" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res?: string): void => {
+        if (res) {
+          if (this.debug) {
+            cmd.log('Response:');
+            cmd.log(res);
+            cmd.log('');
+          }
+
+          const json: ClientSvcResponse = JSON.parse(res);
+          const response: ClientSvcResponseContents = json[0];
+          if (response.ErrorInfo) {
+            cb(new CommandError(response.ErrorInfo.ErrorMessage));
+            return;
+          }
+        }
+
+        delete termGroup._ObjectIdentity_;
+        delete termGroup._ObjectType_;
+        termGroup.Id = termGroup.Id.replace('/Guid(', '').replace(')/', '');
+        termGroup.Description = args.options.description || '';
+        cmd.log(termGroup);
+        cb();
+      }, (err: any): void => this.handleRejectedPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-n, --name <name>',
+        description: 'Name of the term group to add'
+      },
+      {
+        option: '-i, --id [id]',
+        description: 'ID of the term group to add'
+      },
+      {
+        option: '-d, --description [description]',
+        description: 'Description of the term group to add'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.name) {
+        return 'Required option name missing';
+      }
+
+      if (args.options.id) {
+        if (!Utils.isValidGuid(args.options.id)) {
+          return `${args.options.id} is not a valid GUID`;
+        }
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: CommandArgs, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.TERM_GROUP_ADD).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, log in to SharePoint Online tenant admin site,
+    using the ${chalk.blue(commands.LOGIN)} command.
+        
+  Remarks:
+
+    To add a taxonomy term group, you have to first log in to a tenant admin
+    site using the ${chalk.blue(commands.LOGIN)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN} https://contoso-admin.sharepoint.com`)}.
+
+  Examples:
+  
+    Add a new taxonomy term group with the specified name
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_ADD} --name PnPTermSets
+
+    Add a new taxonomy term group with the specified name and id
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_ADD} --name PnPTermSets --id 0e8f395e-ff58-4d45-9ff7-e331ab728beb
+
+    Add a new taxonomy term group with the specified name and description
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_GET} --name PnPTermSets --description 'Term sets for PnP'
+`);
+  }
+}
+
+module.exports = new SpoTermGroupAddCommand();

VelinGeorgiev/office365-cli

src/o365/spo/commands/term/term-group-add.ts
@@ -1,0 +1,258 @@
+import auth from '../../SpoAuth';
+import { ContextInfo, ClientSvcResponse, ClientSvcResponseContents } from '../../spo';
+import config from '../../../../config';
+import * as request from 'request-promise-native';
+const uuidv4 = require('uuid/v4');
+import commands from '../../commands';
+import GlobalOptions from '../../../../GlobalOptions';
+import {
+  CommandError,
+  CommandOption,
+  CommandValidate
+} from '../../../../Command';
+import SpoCommand from '../../SpoCommand';
+import Utils from '../../../../Utils';
+import { TermStore } from './TermStore';
+import { TermGroup } from './TermGroup';
+
+const vorpal: Vorpal = require('../../../../vorpal-init');
+
+interface CommandArgs {
+  options: Options;
+}
+
+interface Options extends GlobalOptions {
+  description?: string;
+  id?: string;
+  name: string;
+}
+
+class SpoTermGroupAddCommand extends SpoCommand {
+  public get name(): string {
+    return commands.TERM_GROUP_ADD;
+  }
+
+  public get description(): string {
+    return 'Adds taxonomy term group';
+  }
+
+  public getTelemetryProperties(args: CommandArgs): any {
+    const telemetryProps: any = super.getTelemetryProperties(args);
+    telemetryProps.description = typeof args.options.id !== 'undefined';
+    telemetryProps.id = typeof args.options.id !== 'undefined';
+    return telemetryProps;
+  }
+
+  protected requiresTenantAdmin(): boolean {
+    return true;
+  }
+
+  public commandAction(cmd: CommandInstance, args: CommandArgs, cb: (err?: any) => void): void {
+    let formDigest: string = '';
+    let termGroup: TermGroup;
+
+    auth
+      .ensureAccessToken(auth.service.resource, cmd, this.debug)
+      .then((accessToken: string): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log(`Retrieved access token ${accessToken}. Retrieving request digest...`);
+        }
+
+        return this.getRequestDigest(cmd, this.debug);
+      })
+      .then((res: ContextInfo): request.RequestPromise => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        formDigest = res.FormDigestValue;
+
+        if (this.verbose) {
+          cmd.log(`Getting taxonomy term store...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': res.FormDigestValue
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="4" ObjectPathId="3" /><ObjectIdentityQuery Id="5" ObjectPathId="3" /><ObjectPath Id="7" ObjectPathId="6" /><ObjectIdentityQuery Id="8" ObjectPathId="6" /><Query Id="9" ObjectPathId="6"><Query SelectAllProperties="true"><Properties /></Query></Query></Actions><ObjectPaths><StaticMethod Id="3" Name="GetTaxonomySession" TypeId="{981cbc68-9edc-4f8d-872f-71146fcbb84f}" /><Method Id="6" ParentId="3" Name="GetDefaultSiteCollectionTermStore" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): request.RequestPromise | Promise<void> => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          return Promise.reject(response.ErrorInfo.ErrorMessage);
+        }
+
+        const termStore: TermStore = json[json.length - 1];
+        const termGroupId: string = args.options.id || uuidv4();
+
+        if (this.verbose) {
+          cmd.log(`Adding taxonomy term group...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': formDigest
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><ObjectPath Id="14" ObjectPathId="13" /><ObjectIdentityQuery Id="15" ObjectPathId="13" /><Query Id="16" ObjectPathId="13"><Query SelectAllProperties="false"><Properties><Property Name="Name" ScalarProperty="true" /><Property Name="Id" ScalarProperty="true" /><Property Name="Description" ScalarProperty="true" /></Properties></Query></Query></Actions><ObjectPaths><Method Id="13" ParentId="6" Name="CreateGroup"><Parameters><Parameter Type="String">${Utils.escapeXml(args.options.name)}</Parameter><Parameter Type="Guid">{${termGroupId}}</Parameter></Parameters></Method><Identity Id="6" Name="${termStore._ObjectIdentity_}" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res: string): request.RequestPromise | Promise<void> => {
+        if (this.debug) {
+          cmd.log('Response:');
+          cmd.log(res);
+          cmd.log('');
+        }
+
+        const json: ClientSvcResponse = JSON.parse(res);
+        const response: ClientSvcResponseContents = json[0];
+        if (response.ErrorInfo) {
+          return Promise.reject(response.ErrorInfo.ErrorMessage);
+        }
+
+        termGroup = json[json.length - 1];
+
+        if (!args.options.description) {
+          return Promise.resolve();
+        }
+
+        if (this.verbose) {
+          cmd.log(`Setting taxonomy term group description...`);
+        }
+
+        const requestOptions: any = {
+          url: `${auth.site.url}/_vti_bin/client.svc/ProcessQuery`,
+          headers: Utils.getRequestHeaders({
+            authorization: `Bearer ${auth.service.accessToken}`,
+            'X-RequestDigest': formDigest
+          }),
+          body: `<Request AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName="${config.applicationName}" xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009"><Actions><SetProperty Id="51" ObjectPathId="45" Name="Description"><Parameter Type="String">${Utils.escapeXml(args.options.description)}</Parameter></SetProperty></Actions><ObjectPaths><Identity Id="45" Name="${termGroup._ObjectIdentity_}" /></ObjectPaths></Request>`
+        };
+
+        if (this.debug) {
+          cmd.log('Executing web request...');
+          cmd.log(requestOptions);
+          cmd.log('');
+        }
+
+        return request.post(requestOptions);
+      })
+      .then((res?: string): void => {
+        if (res) {
+          if (this.debug) {
+            cmd.log('Response:');
+            cmd.log(res);
+            cmd.log('');
+          }
+
+          const json: ClientSvcResponse = JSON.parse(res);
+          const response: ClientSvcResponseContents = json[0];
+          if (response.ErrorInfo) {
+            cb(new CommandError(response.ErrorInfo.ErrorMessage));
+            return;
+          }
+        }
+
+        delete termGroup._ObjectIdentity_;
+        delete termGroup._ObjectType_;
+        termGroup.Id = termGroup.Id.replace('/Guid(', '').replace(')/', '');
+        termGroup.Description = args.options.description || '';
+        cmd.log(termGroup);
+        cb();
+      }, (err: any): void => this.handleRejectedPromise(err, cmd, cb));
+  }
+
+  public options(): CommandOption[] {
+    const options: CommandOption[] = [
+      {
+        option: '-n, --name <name>',
+        description: 'Name of the term group to add'
+      },
+      {
+        option: '-i, --id [id]',
+        description: 'ID of the term group to add'
+      },
+      {
+        option: '-d, --description [description]',
+        description: 'Description of the term group to add'
+      }
+    ];
+
+    const parentOptions: CommandOption[] = super.options();
+    return options.concat(parentOptions);
+  }
+
+  public validate(): CommandValidate {
+    return (args: CommandArgs): boolean | string => {
+      if (!args.options.name) {
+        return 'Required option name missing';
+      }
+
+      if (args.options.id) {
+        if (!Utils.isValidGuid(args.options.id)) {
+          return `${args.options.id} is not a valid GUID`;
+        }
+      }
+
+      return true;
+    };
+  }
+
+  public commandHelp(args: CommandArgs, log: (help: string) => void): void {
+    const chalk = vorpal.chalk;
+    log(vorpal.find(commands.TERM_GROUP_ADD).helpInformation());
+    log(
+      `  ${chalk.yellow('Important:')} before using this command, log in to SharePoint Online tenant admin site,
+    using the ${chalk.blue(commands.LOGIN)} command.
+        
+  Remarks:
+
+    To add a taxonomy term group, you have to first log in to a tenant admin
+    site using the ${chalk.blue(commands.LOGIN)} command,
+    eg. ${chalk.grey(`${config.delimiter} ${commands.LOGIN} https://contoso-admin.sharepoint.com`)}.
+
+  Examples:
+  
+    Add a new taxonomy term group with the specified name
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_ADD} --name PnPTermSets
+
+    Add a new taxonomy term group with the specified name and id
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_ADD} --name PnPTermSets --id 0e8f395e-ff58-4d45-9ff7-e331ab728beb
+
+    Add a new taxonomy term group with the specified name and description
+      ${chalk.grey(config.delimiter)} ${commands.TERM_GROUP_ADD} --name PnPTermSets --description 'Term sets for PnP'
+`);
+  }
+}
+
+module.exports = new SpoTermGroupAddCommand();