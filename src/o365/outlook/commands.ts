4bc6fe66a70e77cfd188d325986f9013a89dc744
Patrick Lamber <patrick@nubo.eu>
2019-12-06 07:19:05 +0100

    Added 'outlook report mailboxusagedetail' command solving #1216

--- src/o365/outlook/commands.ts
+++ src/o365/outlook/commands.ts
@@ -3,6 +3,7 @@ const prefix: string = 'outlook';
 export default {
   OUTLOOK_MAIL_SEND: `${prefix} mail send`,
   OUTLOOK_REPORT_MAILAPPUSAGEVERSIONSUSERCOUNTS: `${prefix} report mailappusageversionsusercounts`,
+  OUTLOOK_REPORT_MAILBOXUSAGEDETAIL: `${prefix} report mailboxusagedetail`,
   OUTLOOK_REPORT_MAILBOXUSAGEMAILBOXCOUNT: `${prefix} report mailboxusagemailboxcount`,
   OUTLOOK_REPORT_MAILBOXUSAGEQUOTASTATUSMAILBOXCOUNTS: `${prefix} report mailboxusagequotastatusmailboxcounts`,
   OUTLOOK_REPORT_MAILBOXUSAGESTORAGE: `${prefix} report mailboxusagestorage`,
